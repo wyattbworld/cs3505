e741cd4e6f331283caa25fb314dd5806c7744efa
wyattbworld <wyattbworld@gmail.com>
2023-09-13 09:16:23 -0600

    Fixed some wave formatting.

--- Facades/sinePDF.cpp
+++ Facades/sinePDF.cpp
@@ -16,17 +16,18 @@ using std::endl;
 int main (){
 
     const char* waveText = "I think this is a good text curve";
-    const double canvasWidth = 610;
+    const double canvasWidth = 580;
     const double canvasHeight = 400;
     const double degreesTwoPi = 360; //Number of degrees in a circle.
+    const double waveHeightPercentage = 0.15; //How much height of the canvas should be occupied by the wave.
     HaruPDF pdf;
-    Sine wave(1.0, 180.0, 360 / strlen (waveText)); //We are going to iterate over all 360 degrees, so we need to add 360 / the number of characters to get our increment.
+    Sine wave(canvasHeight * waveHeightPercentage, degreesTwoPi /2, degreesTwoPi / strlen (waveText)); //We are going to iterate over all 360 degrees, so we need to add 360 / the number of characters to get our increment.
 
     pdf.Open();
 
     for (unsigned int i = 0; i < strlen (waveText); i++)
     {
-        pdf.DrawCharacter(wave.currentAngle() / degreesTwoPi * canvasWidth , canvasHeight/2 - wave.currentHeight() * canvasHeight /6, waveText[i]);
+        pdf.DrawCharacter(wave.currentAngle() / degreesTwoPi * canvasWidth , canvasHeight/2 - wave.currentHeight(), waveText[i]);
         wave++;
     }
 
