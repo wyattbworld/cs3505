2bdc5e20ad15703aed5834a10f0bb9a8bd96f3d5
wyattbworld <wyattbworld@gmail.com>
2023-09-13 10:24:35 -0600

    Fixed error with sine wave being flipped.

--- Facades/sinePDF.cpp
+++ Facades/sinePDF.cpp
@@ -27,14 +27,14 @@ int main (int argc, char **argv){
     const double degreesTwoPi = 360; //Number of degrees in a circle.
     const double waveHeightPercentage = 0.15; //How much height of the canvas should be occupied by the wave.
     HaruPDF pdf;
-    
+
     Sine wave(canvasHeight * waveHeightPercentage, degreesTwoPi /2, degreesTwoPi / strlen (waveText)); //We are going to iterate over all 360 degrees, so we need to add 360 / the number of characters to get our increment.
 
     pdf.Open();
 
     for (unsigned int i = 0; i < strlen (waveText); i++)
     {
-        pdf.DrawCharacter(wave.currentAngle() / degreesTwoPi * canvasWidth , canvasHeight/2 - wave.currentHeight(), waveText[i]);
+        pdf.DrawCharacter(wave.currentAngle() / degreesTwoPi * canvasWidth , canvasHeight/2 + wave.currentHeight(), waveText[i]);
         wave++;
     }
 
